53209ff924d50e8aed6e7c7ad0efa3056c3efdfc
Hakeemshaik1 <130968068+Hakeemshaik1@users.noreply.github.com>
2023-11-30 14:54:33 +0200

    PUSH

---
+++ Assets/LOADSCENE.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LOADSCENE : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LoadNextScene();
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        
+        SceneManager.LoadScene("lv2");
+    }
+}
